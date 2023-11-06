f95e78542e81a37a9ffbe70c7427499581b5d19e
iamMeduu <antun.medved5@gmail.com>
2023-11-06 12:33:37 +0100

    Prvi commit

---
+++ DZ1/ClassLibrary/Episode.cs
@@ -0,0 +1,46 @@
+﻿namespace ClassLibrary
+{
+    public class Episode
+    {
+        private int viewerCount;
+        private double sumOfReviews;
+        private double highestReview;
+        
+        public Episode(int viewerCount, double sumOfReviews, double highestReview)
+        {
+            this.viewerCount = viewerCount;
+            this.sumOfReviews = sumOfReviews;
+            this.highestReview = highestReview;
+
+
+        }
+        public Episode()
+        {
+            this.viewerCount = 0;
+            this.sumOfReviews = 0;
+            this.highestReview= 0;
+        }
+        public int GetViewerCount() {
+            return viewerCount;
+        }
+        public double GetMaxScore()
+        {
+            return highestReview;
+        }
+        public void AddView(double review)
+        {
+            sumOfReviews = sumOfReviews + review;
+            if (review > highestReview)
+            {
+                highestReview = review;
+            }
+            viewerCount++;
+            
+        }
+        public double GetAverageScore()
+        {
+            return sumOfReviews / viewerCount;
+        }
+        
+    }
+}
